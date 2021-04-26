aae43ba1d36a64766f8ee08a78c865374fbf6334
PijushP2610 <82369695+PijushP2610@users.noreply.github.com>
2021-04-26 11:07:22 +0530

    Update Home.js

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -2,8 +2,7 @@ import React from 'react';
 import '../../App.css';
 import STime from '../STime';
 import FTime from '../FTime';
-//import Button from './Button';
-import UpdateTime from '../STime';
+
 function Home () {
     return (
         <>
